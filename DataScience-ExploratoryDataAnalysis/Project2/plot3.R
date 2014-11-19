009139c3b9fa073770231dfe8e9ce92fbbb11bc6
mc <michaelc@mac.com>
2014-11-18 19:00:33 -0800

    bug fix

--- DataScience-ExploratoryDataAnalysis/Project2/plot3.R
+++ DataScience-ExploratoryDataAnalysis/Project2/plot3.R
@@ -4,7 +4,7 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 justonezip <- NEI[NEI$fips=="24510",]
-plot3data <- aggregate(justonezip$Emissions, by = list(year = justonezip$year, type = justonezip$type), FUN = "sum"), data=
+plot3data <- aggregate(justonezip$Emissions, by = list(year = justonezip$year, type = justonezip$type), FUN = "sum")
 png(file="plot3.png")
 qplot(year, x, data=plot3data, facets = . ~ type)
 dev.off() 
