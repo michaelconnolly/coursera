6e2e1cb0ccfbb9b2d6462baaf5d6749ec49f0523
mc <michaelc@mac.com>
2014-11-20 21:51:15 -0800

    finished up EDA project 2

--- DataScience-ExploratoryDataAnalysis/Project2/plot2.R
+++ DataScience-ExploratoryDataAnalysis/Project2/plot2.R
@@ -1,9 +1,17 @@
+## Load our data!
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+## Filter down to Baltimore city, then grab the sums of each year.
 justonezip <- NEI[NEI$fips=="24510",]
 plot2data <- aggregate(justonezip$Emissions, by = list(year = justonezip$year), FUN = "sum")
 
 png(file="plot2.png")
 barplot(plot2data$x, names.arg=plot2data$year)
+dev.off()
+## Bust out the chart.
+png(file="plot2.png")
+opt <- options("scipen" = 20)
+barplot(plot2data$x, names.arg=plot2data$year, main="Total PM25 Emissions in Baltimore City")
+options(opt)
 dev.off()
--- DataScience-ExploratoryDataAnalysis/Project2/plot4.R
+++ DataScience-ExploratoryDataAnalysis/Project2/plot4.R
@@ -9,19 +9,15 @@ SCC <- readRDS("Source_Classification_Code.rds")
 names(SCC)[4] <- "EISector"
 
 ## add the activities labels and merge up the training data.
-merged_table2 <- sqldf("SELECT NEI.*, SCC.EISector FROM NEI LEFT JOIN SCC ON NEI.SCC=SCC.SCC")
-merged_table3 <- sqldf("SELECT * FROM merged_table2 WHERE EISector LIKE '%Coal%' ")
+merged_table4 <- sqldf("SELECT NEI.*, SCC.EISector FROM NEI LEFT JOIN SCC ON NEI.SCC=SCC.SCC")
+merged_table4 <- sqldf("SELECT * FROM merged_table4 WHERE EISector LIKE '%Coal%' ")
 
-
-##training_label_descrip$id = seq(1, nrow(training_label_descrip))
-##training_data$id = seq(1, nrow(training_labels))
-##training_subject$id = seq(1, nrow(training_subject))
-##training_all <- sqldf("SELECT training_subject.subject_id, training_label_descrip.activity_name, training_data.* FROM training_subject, training_label_descrip, training_data WHERE training_subject.id = training_label_descrip.id AND training_subject.id = training_data.id")
-
+## Question: Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
 
 ##justonezip <- NEI[NEI$fips=="24510",]
-plot4data <- aggregate(merged_table3$Emissions, by = list(year = merged_table3$year), FUN = "sum")
+plot4data <- aggregate(merged_table4$Emissions, by = list(year = merged_table4$year), FUN = "sum")
 png(file="plot4.png")
-#qplot(year, x, data=plot4data)
-barplot(plot4data$x, names.arg=plot4data$year)
+opt <- options("scipen" = 20)
+barplot(plot4data$x, names.arg=plot4data$year, main="Coal combusion-related Emissions in USA")
+options(opt)
 dev.off() 
