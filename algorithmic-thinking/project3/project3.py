f7356643dcbcfb01920ab9aee9783e0ad9e9dfa8
mc <michaelc@mac.com>
2015-07-24 14:10:22 -0700

    some edits

--- algorithmic-thinking/project3/project3.py
+++ algorithmic-thinking/project3/project3.py
@@ -6,136 +6,9 @@ Project 3
 
 import math
 import alg_cluster
-
-##class Cluster:
-##    """
-##    Class for creating and merging clusters of counties
-##    """
-##    
-##    def __init__(self, fips_codes, horiz_pos, vert_pos, population, risk):
-##        """
-##        Create a cluster based the models a set of counties' data
-##        """
-##        self._fips_codes = fips_codes
-##        self._horiz_center = horiz_pos
-##        self._vert_center = vert_pos
-##        self._total_population = population
-##        self._averaged_risk = risk
-##        
-##        
-##    def __repr__(self):
-##        """
-##        String representation assuming the module is "alg_cluster".
-##        """
-##        rep = "alg_cluster.Cluster("
-##        rep += str(self._fips_codes) + ", "
-##        rep += str(self._horiz_center) + ", "
-##        rep += str(self._vert_center) + ", "
-##        rep += str(self._total_population) + ", "
-##        rep += str(self._averaged_risk) + ")"
-##        return rep
-##
-##
-##    def fips_codes(self):
-##        """
-##        Get the cluster's set of FIPS codes
-##        """
-##        return self._fips_codes
-##    
-##    def horiz_center(self):
-##        """
-##        Get the averged horizontal center of cluster
-##        """
-##        return self._horiz_center
-##    
-##    def vert_center(self):
-##        """
-##        Get the averaged vertical center of the cluster
-##        """
-##        return self._vert_center
-##    
-##    def total_population(self):
-##        """
-##        Get the total population for the cluster
-##        """
-##        return self._total_population
-##    
-##    def averaged_risk(self):
-##        """
-##        Get the averaged risk for the cluster
-##        """
-##        return self._averaged_risk
-##   
-##        
-##    def copy(self):
-##        """
-##        Return a copy of a cluster
-##        """
-##        copy_cluster = Cluster(set(self._fips_codes), self._horiz_center, self._vert_center,
-##                               self._total_population, self._averaged_risk)
-##        return copy_cluster
-##
-##
-##    def distance(self, other_cluster):
-##        """
-##        Compute the Euclidean distance between two clusters
-##        """
-##        vert_dist = self._vert_center - other_cluster.vert_center()
-##        horiz_dist = self._horiz_center - other_cluster.horiz_center()
-##        return math.sqrt(vert_dist ** 2 + horiz_dist ** 2)
-##        
-##    def merge_clusters(self, other_cluster):
-##        """
-##        Merge one cluster into another
-##        The merge uses the relatively populations of each
-##        cluster in computing a new center and risk
-##        
-##        Note that this method mutates self
-##        """
-##        if len(other_cluster.fips_codes()) == 0:
-##            return self
-##        else:
-##            #print "start:", self._fips_codes
-##            #print "other:", other_cluster.fips_codes()
-##            self._fips_codes.update(set(other_cluster.fips_codes()))
-##            #print "end:", self._fips_codes
-##            
-##            # compute weights for averaging
-##            self_weight = float(self._total_population)                        
-##            other_weight = float(other_cluster.total_population())
-##            self._total_population = self._total_population + other_cluster.total_population()
-##            self_weight /= self._total_population
-##            other_weight /= self._total_population
-##                    
-##            # update center and risk using weights
-##            self._vert_center = self_weight * self._vert_center + other_weight * other_cluster.vert_center()
-##            self._horiz_center = self_weight * self._horiz_center + other_weight * other_cluster.horiz_center()
-##            self._averaged_risk = self_weight * self._averaged_risk + other_weight * other_cluster.averaged_risk()
-##            return self
-##
-##    def cluster_error(self, data_table):
-##        """
-##        Input: data_table is the original table of cancer data used in creating the cluster.
-##        
-##        Output: The error as the sum of the square of the distance from each county
-##        in the cluster to the cluster center (weighted by its population)
-##        """
-##        # Build hash table to accelerate error computation
-##        fips_to_line = {}
-##        for line_idx in range(len(data_table)):
-##            line = data_table[line_idx]
-##            fips_to_line[line[0]] = line_idx
-##        
-##        # compute error as weighted squared distance from counties to cluster center
-##        total_error = 0
-##        counties = self.fips_codes()
-##        for county in counties:
-##            line = data_table[fips_to_line[county]]
-##            singleton_cluster = Cluster(set([line[0]]), line[1], line[2], line[3], line[4])
-##            singleton_distance = self.distance(singleton_cluster)
-##            total_error += (singleton_distance ** 2) * singleton_cluster.total_population()
-##        return total_error
-
+import random
+import time
+import matplotlib.pyplot as plt
 
             
 def slow_closest_pair(cluster_list):
@@ -443,6 +316,57 @@ def set_of_county_tuples(cluster_list):
         county_tuple = tuple(sorted(list(counties_in_cluster)))
         set_of_clusters.add(county_tuple)
     return set_of_clusters
+
+
+def gen_random_clusters(num_clusters):
+
+    cluster_list = list([])
+
+    for cluster_index in range(0, num_clusters):
+
+        horiz = random.uniform(-1, 1)
+        vert = random.uniform(-1, 1)
+        cluster = alg_cluster.Cluster(set(), horiz, vert, 0, 0)
+        cluster_list.append(cluster)
+
+    return cluster_list
+    
+
+def question_one():
+
+    clusters = dict()
+    times_slow = dict()
+    times_fast = dict()
+
+    for num_clusters in range(2, 15):
+        
+        cluster_list = gen_random_clusters(num_clusters)
+        clusters[num_clusters] = cluster_list
+
+        time_start = time.time()
+        foo = slow_closest_pair(cluster_list)
+        time_end = time.time() 
+        time_total = time_end - time_start
+        times_slow[num_clusters] = time_total
+
+        time_start = time.time()
+        foo = fast_closest_pair(cluster_list)
+        time_end = time.time()
+        time_total = time_end - time_start
+        times_fast[num_clusters] = time_total
+        
+    print times_slow
+    print times_fast
+
+    plt.plot(times_slow.keys(), times_slow.values(), '-b', label="slow_closest_pair")
+    plt.plot(times_fast.keys(), times_fast.values(), '-r', label="fast_closest_pair")
+    #plt.plot(list_index, resilience_upa, '-g', label="upa m=3")
+    #plt.plot(list_index, resilient_line, '-y', label="resilient_line")
+    plt.xlabel('number of clusters')
+    plt.ylabel('runtime')
+        
+
+time_trials()
 
 
 ###
